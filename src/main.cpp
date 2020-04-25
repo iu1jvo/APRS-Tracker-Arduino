ea26782c7f3a2dc5d5de4c9aef104acd5e9bd93b
IU1JVO <iu1jvo@gmail.com>
2020-04-26 00:01:21 +0200

    Modify and Test LibAPRS: -)Ability to configure and use different pin/port.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,72 @@
 #include <Arduino.h>
 #include "TinyGPS.h"
 #include "LibAPRS.h"
+#include "SoftwareSerial.h"
 
+static SoftwareSerial ss = SoftwareSerial(PIN_SPI_MISO, PIN_SPI_MOSI);
+
+#define ADC_REFERENCE REF_5V
+// You can also define whether your modem will be
+// running with an open squelch radio:
+#define OPEN_SQUELCH false
+
+
+// You always need to include this function. It will
+// get called by the library every time a packet is
+// received, so you can process incoming packets.
+//
+// If you are only interested in transmitting, you
+// should just leave this function empty.
+// 
+// IMPORTANT! This function is called from within an
+// interrupt. That means that you should only do things
+// here that are FAST. Don't print out info directly
+// from this function, instead set a flag and print it
+// from your main loop, like this:
+
+boolean gotPacket = false;
+AX25Msg incomingPacket;
+uint8_t *packetData;
+void aprs_msg_callback(struct AX25Msg *msg) {
+/*   // If we already have a packet waiting to be
+  // processed, we must drop the new one.
+  if (!gotPacket) {
+    // Set flag to indicate we got a packet
+    gotPacket = true;
+
+    // The memory referenced as *msg is volatile
+    // and we need to copy all the data to a
+    // local variable for later processing.
+    memcpy(&incomingPacket, msg, sizeof(AX25Msg));
+
+    // We need to allocate a new buffer for the
+    // data payload of the packet. First we check
+    // if there is enough free RAM.
+    if (freeMemory() > msg->len) {
+      packetData = (uint8_t*)malloc(msg->len);
+      memcpy(packetData, msg->info, msg->len);
+      incomingPacket.info = packetData;
+    } else {
+      // We did not have enough free RAM to receive
+      // this packet, so we drop it.
+      gotPacket = false;
+    }
+  }
+ */}
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
 
 void setup() {
-  // put your setup code here, to run once:
+
+   // Initialise APRS library - This starts the modem
+  APRS_init(ADC_REFERENCE, OPEN_SQUELCH);
+
 }
 
 void loop() {
+  
   // put your main code here, to run repeatedly:
+  APRS_poll();
+
 }
